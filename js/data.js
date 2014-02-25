98ea07b0b475178adda3ebe9719a7507df67cd04
Håkon Bråten <haakbraa@gmail.com>
2014-02-25 17:47:45 +0100

    Lagt til mulighet for å legge til romnavn og personer

--- js/data.js
+++ js/data.js
@@ -46,6 +46,9 @@ function Line(a, b) {
 	this.b = b;
 	this.dist = a.ll.distanceTo(b.ll);
 	this.polyline = L.polyline([a.ll, b.ll]);
+
+	a.neighbours.push(b.id);
+	b.neighbours.push(a.id);
 }
 
 Line.prototype.write = function() {
@@ -58,6 +61,8 @@ function Node(ll, f, id) {
 	this.ll = ll;
 	this.floor = f;
 	this.type = null;
+	this.persons = new Array();
+	this.names = new Array();
 	if (id == null)
 		this.write();
 	this.neighbours = new Array();
@@ -72,13 +77,73 @@ Node.prototype.write = function() {
 
 // Data class
 
-function Data(map) {
+function Data(map, info) {
 	this.map = map;
+	this.info = info;
+	this.mode = 0;
 	this.floor = 0;
 	this.maps = new Array();
 	this.nodes = new Object();
 	this.lines = new Array();
 	this.selectedNode = null;
+	
+	this.updateInfo();
+}
+
+function addPerson(sn) {
+	var name = prompt("Person til node " + sn, "");
+	if (name == null)
+		return;
+	execute("INSERT INTO person VALUES (" + sn + ", \"" + name + "\")");
+}
+function addRoom(sn) {
+	var name = prompt("Navn til node " + sn, "");
+	if (name == null)
+		return;
+	execute("INSERT INTO room VALUES (" + sn + ", \"" + name + "\")");
+}
+
+Data.prototype.updateInfo = function() {
+	if (this.selectedNode == null) {
+		var html = "Velg rom";
+	} else {
+		var node = this.nodes[this.selectedNode];
+
+		var html = (this.mode <= 0) ? "Ingen navn" : "Node " + this.selectedNode;
+		if (node.names.length > 0) {
+			html = "<b>" + node.names[0] + "</b>";
+		}
+		
+		if (this.mode == 1) // info mode
+			html += "<button onclick=\"addRoom("+this.selectedNode+")\">+</button>";
+
+
+		if (node.names.length > 1) {
+			for (var i = 1; i < node.names.length; i++)
+				html += "/<br><b>" + node.names[i] + "</b>";
+		}
+
+		html += "<br>";
+
+		html += "<br><u>Personer:</u>";
+		if (this.mode == 1) // info mode
+			html += "<button onclick=\"addPerson("+this.selectedNode+")\">+</button>";
+
+		if (node.persons.length > 0) {
+			for (var i = 0; i < node.persons.length; i++)
+				html += "<br>" + node.persons[i];
+		}
+	}
+
+	html += "<br><br>";
+	if (this.mode == 0)
+		html += "<small>read</small>";		
+	if (this.mode == 1)
+		html += "<small>info</small>";		
+	if (this.mode == 2)
+		html += "<small>line</small>";
+
+	this.info._container.innerHTML = html;
 }
 
 Data.prototype.read = function() {
@@ -98,7 +163,18 @@ Data.prototype.read = function() {
 	for (var i = 0; i < lines.length; i++) {
 		var l = new Line(this.nodes[lines[i].node_a], this.nodes[lines[i].node_b], lines[i].dist);
 		this.lines.push(l);
-	}	
+	}
+
+	var persons = getAll("SELECT * FROM person");
+	for (var i = 0; i < persons.length; i++) {
+		this.nodes[persons[i]["node_id"]].persons.push(persons[i]["name"]);
+	}
+
+	var names = getAll("SELECT * FROM room");
+	for (var i = 0; i < names.length; i++) {
+		this.nodes[names[i]["node_id"]].names.push(names[i]["name"]);
+	}
+	
 
 	this.addFloor();
 }
@@ -117,14 +193,18 @@ Data.prototype.selectNode = function(id) {
 		this.nodes[id].marker.setIcon(redIcon);
 	}
 	else {
-		var l = new Line(this.nodes[this.selectedNode], this.nodes[id]);
-		l.write();
-		this.lines.push(l);
-		l.polyline.addTo(this.map);
+		if (this.mode == 2) { // Line mode
+			var l = new Line(this.nodes[this.selectedNode], this.nodes[id]);
+			l.write();
+			this.lines.push(l);
+			l.polyline.addTo(this.map);
+		}
 		this.nodes[id].marker.setIcon(greenIcon);
 		this.nodes[this.selectedNode].marker.setIcon(redIcon);
 		this.selectedNode = id;
 	}
+
+	this.updateInfo();
 }
 
 Data.prototype.getClosestNode = function(target, limit) {
@@ -144,10 +224,17 @@ Data.prototype.getClosestNode = function(target, limit) {
 
 
 Data.prototype.clickHandler = function(e) {
+	if (e.originalEvent.altKey && this.mode != -1) {
+		this.mode = (this.mode + 1) % 3;
+		this.updateInfo();
+		this.removeFloor();
+		this.addFloor();
+		return;
+	}
 	var nearestNode = this.getClosestNode(e.latlng, 40);
 	if (nearestNode != null)
 		this.selectNode(nearestNode.id);
-	else
+	else if(this.mode > 0) // 0 = readMode
 		this.addNode(e);
 }
 
@@ -160,8 +247,17 @@ Data.prototype.addNode = function(e) {
 Data.prototype.addFloor = function() {
 	this.map.addLayer(this.maps[this.floor]);
 	for (var id in this.nodes)
-		if (this.nodes[id].floor == this.floor)
-			this.map.addLayer(this.nodes[id].marker);
+		if (this.nodes[id].floor == this.floor) {
+			if (this.mode == -1) {
+				if (this.nodes[id].names.length > 0) {
+					this.map.addLayer(this.nodes[id].marker);
+				}
+			} else {
+					this.map.addLayer(this.nodes[id].marker);
+			}
+		}
+	if (this.mode != 2) // line mode
+		return;
 	for (var i = 0; i < this.lines.length; i++) {
 		if (this.lines[i].a.floor == this.floor || this.lines[i].b.floor == this.floor)
 			this.lines[i].polyline.addTo(map); 
--- js/leaflet_util.js
+++ js/leaflet_util.js
@@ -1,24 +1,41 @@
+// Custom objects
+
+
+var CustomControl = function(name, options) {
+	
+	var control = new (L.Control.extend({
+		options: options,
+		onAdd: function (map) {
+			var container = L.DomUtil.create('div',name);
+			container.innerHTML = "Hello";
+			return container;
+		}
+	}));
+
+	return control;
+}
+
+
 var Button = function(buttonFunction, options) {
 
 	var control = new (L.Control.extend({
-	options: { position: 'topright' },
-	onAdd: function (map) {
-		controlDiv = L.DomUtil.create('div', 'button');
-		L.DomEvent
-			.addListener(controlDiv, 'click', L.DomEvent.stopPropagation)
-			.addListener(controlDiv, 'click', L.DomEvent.preventDefault)
-			.addListener(controlDiv, 'click', this.clickFunction);
+		options: { position: 'topright' },
+		onAdd: function (map) {
+			controlDiv = L.DomUtil.create('div', 'button');
+			L.DomEvent
+				.addListener(controlDiv, 'click', L.DomEvent.stopPropagation)
+				.addListener(controlDiv, 'click', L.DomEvent.preventDefault)
+				.addListener(controlDiv, 'click', this.clickFunction);
 
-		// Set CSS for the control border
-		var controlUI = L.DomUtil.create('div', 'button-border', controlDiv);
-		controlUI.title = 'Click for Hello World!';
+			// Set CSS for the control border
+			var controlUI = L.DomUtil.create('div', 'button-border', controlDiv);
 
-		// Set CSS for the control interior
-		var controlText = L.DomUtil.create('div', 'button-interior', controlUI);
-		controlText.innerHTML = options['text'];
+			// Set CSS for the control interior
+			var controlText = L.DomUtil.create('div', 'button-interior', controlUI);
+			controlText.innerHTML = options['text'];
 
-		return controlDiv;
-	}
+			return controlDiv;
+		}
 	}));
 
 	control.clickFunction = buttonFunction;
